e12054ed53d2a0454983e05699b75d5d84272140
Nixon2223 <n.nixon@hotmail.co.uk>
2021-11-24 11:00:58 +0000

    customer_has_name passed

--- src/customer.py
+++ src/customer.py
@@ -1,2 +1,5 @@
 class Customer:
     def __init__(self, id, belly, wallet):
+        self.id = id
+        self.belly = belly
+        self.wallet = wallet
--- tests/pub_test.py
+++ tests/pub_test.py
@@ -1,12 +1,16 @@
 import unittest
 from src.pub import Pub
+from src.drink import Drink
+from src.customer import Customer
 
 class TestPub(unittest.TestCase):
     
     def setUp(self):
         self.pub = Pub("The Prancing Pony", 100.00)
-
+        self.customer = Customer(1, 0, 15.55)
+        
     def test_pub_has_name(self):
         self.assertEqual("The Prancing Pony", self.pub.name)
     
-    def
+    def test_customer_has_id(self):
+        self.assertEqual(1, self.customer.id)
